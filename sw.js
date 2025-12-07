4483523bee56847d82f592bebf19786e1e74e78b
zwsh-cmd <ziwush@gmail.com>
2025-12-07 22:14:49 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'scriptmind-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './main.js',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
